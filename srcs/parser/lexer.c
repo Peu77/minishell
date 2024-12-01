2feb30f3d10752228c040f7d55d67c056b23b82d
Emil Ebert <emilebert@icloud.com>
2024-12-01 13:20:18 +0100

    basic lexer

--- srcs/parser/lexer.c
+++ srcs/parser/lexer.c
@@ -6,33 +6,138 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 16:34:29 by eebert            #+#    #+#             */
-/*   Updated: 2024/11/30 16:41:36 by eebert           ###   ########.fr       */
+/*   Updated: 2024/12/01 13:19:36 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 #include "libft.h"
+#include <stdbool.h>
+#include <stdio.h>
 
-static t_token_type get_token_type(char* c)
+static t_token_type get_token_type(const char* str)
 {
-    if(*c == '|')
+    if(*str == '|')
         return TOKEN_PIPE;
-    if(*c == ';')
+    if(*str == ';')
         return TOKEN_SEMICOLON;
-    if(ft_strncmp(c, "&&", 2) == 0)
+    if(ft_strncmp(str, "&&", 2) == 0)
         return TOKEN_AND;
-    if(ft_strncmp(c, "||", 2) == 0)
+    if(ft_strncmp(str, "||", 2) == 0)
         return TOKEN_OR;
-    if(*c == '<')
+    if(*str == '<')
         return TOKEN_REDIRECT_INPUT;
-    if(*c == '>')
+    if(*str == '>')
         return TOKEN_REDIRECT_OUTPUT;
-    if(ft_strncmp(c, ">>", 2) == 0)
+    if(ft_strncmp(str, ">>", 2) == 0)
         return TOKEN_REDIRECT_APPEND;
-    if(*c == '(')
+    if(*str == '(')
         return TOKEN_PARENTHESES_OPEN;
-    if(*c == ')')
+    if(*str == ')')
         return TOKEN_PARENTHESES_CLOSE;
-    if(*c == '\0')
+    if(ft_isalnum(*str))
+        return TOKEN_STRING;
+    if(*str == '\0')
         return TOKEN_END;
+    return TOKEN_NONE;
 }
+
+static bool add_token(t_list** tokens, t_token_type type, char* value)
+{
+    t_ast_node* node;
+
+    node = malloc(sizeof(t_ast_node));
+    if(!node)
+        return false;
+
+    node->type = type;
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+    ft_lstadd_back(tokens, ft_lstnew(node));
+    return true;
+}
+
+void lex_tokens(char *input, t_list** tokens)
+{
+    t_token_type type;
+    int i;
+    bool in_string;
+    bool in_quotes;
+    int string_i;
+
+    in_string = false;
+    in_quotes = false;
+    i = 0;
+    string_i = 0;
+    while(input[i])
+    {
+        if(ft_isspace(input[i]))
+        {
+            i++;
+            continue;
+        }
+        string_i = 0;
+        if(input[i] == '\'') {
+            // TODO: handle escaped quotes
+            while(input[i + string_i + 1] && input[i + string_i + 1] != '\'')
+                string_i++;
+
+            add_token(tokens, TOKEN_STRING, ft_substr(input, i + 1, string_i - 1));
+            i += string_i + 2;
+        }
+
+        string_i = 0;
+        while(ft_isalpha(input[i + string_i]))
+            string_i++;
+
+        if(string_i > 0)
+        {
+            add_token(tokens, TOKEN_STRING, ft_substr(input, i, string_i));
+            i += string_i;
+        }
+
+        type = get_token_type(input + i);
+        if(type != TOKEN_NONE) {
+            add_token(tokens, type, NULL);
+            if(type == TOKEN_END)
+                break;
+            i++;
+            // skip second character if it's a double character token
+            if(type >= TOKEN_AND) {
+                i++;
+            }
+        }
+    }
+}
+
+static void print_tokens(t_list* tokens)
+{
+    t_ast_node* node;
+
+    while(tokens)
+    {
+        node = tokens->content;
+        if(node->type == TOKEN_STRING)
+            printf("TOKEN_STRING: %s\n", node->value);
+        else
+            printf("TOKEN: %d\n", node->type);
+        tokens = tokens->next;
+    }
+}
+
+/*
+ * compile with: cc lexer.c ../../libft/libft.a -I ../../includes && ./a.out
+ *
+int main() {
+    t_list* tokens = NULL;
+    char* input = "echo hello && echo 'test && echo () test > || test' () world";
+
+    lex_tokens(input, &tokens);
+
+
+    print_tokens(tokens);
+
+    return 0;
+}
+*/
