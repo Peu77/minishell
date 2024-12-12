885bf55cf70b78332132697e5203c41d98aef1d7
Florent Tapponnier <ftapponn@1-E-1.42heilbronn.de>
2024-12-12 18:07:52 +0100

    Exit command a bit ameliorate

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -1,12 +1,24 @@
 #include "../../includes/minishell.h"
 
+
 int exit_command(t_command_test *command)
 {
-	char **cmd = ft_split(command->argument, ' ');
-	if(cmd[0] && cmd[1])
+	char **cmd; 
+
+	if(command->argument == NULL)
+		exit(EXIT_SUCCESS);
+	cmd = ft_split(command->argument, ' ');
+	if(!cmd)
+		return pec(ERROR_SPLIT);	
+	if (cmd[1]) 
 	{
-		pev("Error, too many arguments");
-		return (1);
+		pev("Error, too many arguments for exit command");
+		free_command_split(cmd);
+		return 1;
 	}
-	exit(EXIT_SUCCESS);
+	else
+	{
+		free_command_split(cmd);
+		exit(EXIT_SUCCESS);
+	}
 }
