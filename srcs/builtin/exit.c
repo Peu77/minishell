8620014bbffd8968b944f24f9b9062ee534facd4
Florent Tapponnier <ftapponn@3-F-6.42heilbronn.de>
2024-12-13 14:58:34 +0100

    exit command

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -1,21 +1,54 @@
 #include "../../includes/minishell.h"
 
 
-int exit_command(t_command_test *command)
+long ft_atol(const char *str)
 {
-	char **cmd; 
+    long result = 0;
+    int sign = 1;
 
-	if(command->argument == NULL)
-		exit(EXIT_SUCCESS);
-	cmd = ft_split(command->argument, ' ');
-	if(!cmd)
-		return pec(ERROR_SPLIT);	
-	if (cmd[1]) 
-	{
-		pev("Error, too many arguments for exit command");
-		free_command_split(cmd);
-		return 1;
-	}
-	return (1);
+    while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r' || *str == '\v' || *str == '\f')
+        str++;
+    if (*str == '-')
+    {
+        sign = -1;
+        str++;
+    }
+    else if (*str == '+')
+        str++;
+    while (ft_isdigit(*str))
+    {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    return (result * sign);
 }
 
+int exit_command(t_command_test *command)
+{
+    char **cmd;
+    long exit_code;
+    int i = -1;
+
+    if (command->argument == NULL)
+        exit(EXIT_SUCCESS);
+    cmd = ft_split(command->argument, ' ');
+    if (!cmd)
+        return pec(ERROR_SPLIT);
+    if (cmd[1])
+    {
+		free_command_split(cmd);
+        return pec(ERROR_TOO_ARGUMENT);
+    }
+    while (cmd[0][++i] != '\0')
+    {
+        if (!ft_isdigit(cmd[0][i]))
+        {
+            free_command_split(cmd);
+            return pec(ERROR_DIGIT_ARGUMENT);
+        }
+	}
+    exit_code = ft_atol(cmd[0]) % 255;
+	printf("Exit code : %d \n", (int)exit_code);
+    free_command_split(cmd);
+    exit((int)exit_code);
+}
--- srcs/monitor/and_or_monitor.c
+++ srcs/monitor/and_or_monitor.c
@@ -1,25 +1,25 @@
 #include "../../includes/minishell.h"
 
-int and_monitor(t_ast_node *node, t_command_test *command, char **envp, t_env *env)
+int and_monitor(t_ast_node *node, t_command_test *command, t_env *env)
 {
     int left_result = 0;
     
     if (node->left)
-        left_result = tree_monitor(node->left, command, envp, env);
+        left_result = tree_monitor(node->left, command, env);
     if (!left_result && node->right)
-        return tree_monitor(node->right, command, envp, env);
+        return tree_monitor(node->right, command, env);
     
     return left_result;
 }
 
-int or_monitor(t_ast_node *node, t_command_test *command, char **envp, t_env *env)
+int or_monitor(t_ast_node *node, t_command_test *command, t_env *env)
 {
     int left_result = 0;
 
     if (node->left)
-        left_result = tree_monitor(node->left, command, envp, env);
+        left_result = tree_monitor(node->left, command, env);
     if (left_result && node->right)
-        return tree_monitor(node->right, command, envp, env);
+        return tree_monitor(node->right, command, env);
     
     return left_result;
 }
