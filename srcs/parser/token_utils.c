e6eeac3727e230be7edaaf3166284f192c149522
Emil Ebert <emilebert@icloud.com>
2025-01-14 17:19:41 +0100

    token_utils norminette

--- srcs/parser/token_utils.c
+++ srcs/parser/token_utils.c
@@ -6,30 +6,32 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 16:20:45 by eebert            #+#    #+#             */
-/*   Updated: 2024/12/11 15:48:51 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/14 17:19:18 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-void free_token(void *content) {
-    t_token *token;
+void	free_token(void *content)
+{
+	t_token	*token;
 
-    token = content;
-    free(token->value);
-    if(is_redirect_token(token->type) && token->data)
-        free_redirect(token->data);
-    free(token);
+	token = content;
+	free(token->value);
+	if (is_redirect_token(token->type) && token->data)
+		free_redirect(token->data);
+	free(token);
 }
 
-t_token* create_token(t_token_type type, char* value, void* data) {
-    t_token *token;
+t_token	*create_token(t_token_type type, char *value, void *data)
+{
+	t_token	*token;
 
-    token = malloc(sizeof(t_token));
-    if(!token)
-        return NULL;
-    token->type = type;
-    token->value = value;
-    token->data = data;
-    return token;
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->type = type;
+	token->value = value;
+	token->data = data;
+	return (token);
 }
