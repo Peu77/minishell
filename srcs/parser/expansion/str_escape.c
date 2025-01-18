9bb72d129103a419c4b22e25ee5beaebfaf5227a
Emil Ebert <emilebert@icloud.com>
2025-01-19 00:34:00 +0100

    fix splitter

--- srcs/parser/expansion/str_escape.c
+++ srcs/parser/expansion/str_escape.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 22:56:08 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/18 23:48:22 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/19 00:31:02 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,54 @@ bool is_escaped(const char *str, int i)
     return (backslashes % 2);
 }
 
-char	*ft_unescape_string(char *str)
+char *ft_unescape_string(char *str)
 {
-    int	i;
-    int	j;
-    int	len;
+    int i;
+    int j;
+    int len;
+    char quote_type;
+    bool in_quotes;
 
     if (!str)
         return (NULL);
     len = strlen(str);
     i = 0;
     j = 0;
+    in_quotes = false;
+    quote_type = 0;
     while (i < len)
     {
         if (str[i] == '\\' && i + 1 < len)
-            i++;
-        else if ((str[i] == '"' || str[i] == '\'') &&
-            (i == 0 || str[i - 1] != '\\'))
         {
-            i++;
-            continue ;
+            if (str[i + 1] == '\\')
+            {
+                str[j++] = '\\';
+                i += 2;
+            }
+            else
+            {
+                str[j++] = str[i + 1];
+                i += 2;
+            }
         }
-        str[j] = str[i];
-        i++;
-        j++;
+        else if ((str[i] == '"' || str[i] == '\'') && (i == 0 || str[i - 1] != '\\'))
+        {
+            if (!in_quotes)
+            {
+                in_quotes = true;
+                quote_type = str[i];
+                str[j++] = str[i++];
+            }
+            else if (str[i] == quote_type)
+            {
+                in_quotes = false;
+                str[j++] = str[i++];
+            }
+            else
+                str[j++] = str[i++];
+        }
+        else
+            str[j++] = str[i++];
     }
     str[j] = '\0';
     return (str);
