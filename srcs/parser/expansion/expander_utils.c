885a2e34b8b3ed20dc4bff88d5089eb2da1b2d2d
Emil Ebert <emilebert@icloud.com>
2025-01-18 23:42:58 +0100

    refactor code

--- srcs/parser/expansion/expander_utils.c
+++ srcs/parser/expansion/expander_utils.c
@@ -12,13 +12,6 @@
 
 #include <minishell.h>
 
-int	*get_char_count(void)
-{
-	static int	char_count = 0;
-
-	return (&char_count);
-}
-
 bool	handle_tilde_expansion(int *i, t_list **result_chars)
 {
 	t_list	*new_node;
@@ -35,22 +28,6 @@ bool	handle_tilde_expansion(int *i, t_list **result_chars)
 	if (!new_node)
 		return (pe(ERROR_MALLOC), false);
 	(*get_char_count()) += ft_strlen(str_cpy);
-	(*i)++;
-	return (ft_lstadd_back(result_chars, new_node), true);
-}
-
-bool	add_char_to_result(const char *str, int *i, t_list **result_chars)
-{
-	char	*str_cpy;
-	t_list	*new_node;
-
-	str_cpy = ft_substr(str, *i, 1);
-	if (!str_cpy)
-		return (pe(ERROR_MALLOC), false);
-	new_node = ft_lstnew(str_cpy);
-	if (!new_node)
-		return (free(str_cpy), pe(ERROR_MALLOC), false);
-	(*get_char_count())++;
 	(*i)++;
 	return (ft_lstadd_back(result_chars, new_node), true);
 }
