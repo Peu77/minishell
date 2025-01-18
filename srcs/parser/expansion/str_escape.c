5523f1cd0af3b00f68ae07b9fb9c1d3a214e9954
Emil Ebert <emilebert@icloud.com>
2025-01-18 23:49:36 +0100

    remove debug message and refactor code by moving is_escaped function it's own file

--- srcs/parser/expansion/str_escape.c
+++ srcs/parser/expansion/str_escape.c
@@ -6,11 +6,24 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 22:56:08 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/18 23:03:48 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/18 23:48:22 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+bool is_escaped(const char *str, int i)
+{
+    size_t backslashes;
+
+    backslashes = 0;
+    while (i >= 0 && str[i - 1] == '\\')
+    {
+        backslashes++;
+        i--;
+    }
+    return (backslashes % 2);
+}
 
 char	*ft_unescape_string(char *str)
 {
