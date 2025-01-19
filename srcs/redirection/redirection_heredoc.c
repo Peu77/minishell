b7b1ae6417dd0a91047d26ca033558769a81a0a1
Florent Tapponnier <ftapponn@3-I-5.42heilbronn.de>
2025-01-19 19:33:37 +0100

    ctrl +d heredoc

--- srcs/redirection/redirection_heredoc.c
+++ srcs/redirection/redirection_heredoc.c
@@ -6,12 +6,13 @@
 /*   By: ftapponn <ftapponn@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 21:08:25 by ftapponn          #+#    #+#             */
-/*   Updated: 2025/01/19 19:15:47 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/19 19:31:52 by ftapponn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/*
 static void	cleanup_and_exit(int temp_fd, t_command *command,
 		t_parenthesis_fd *parenthesis_fd)
 {
@@ -29,7 +30,8 @@ static void	cleanup_and_exit(int temp_fd, t_command *command,
 	unlink("heredoc_temp.txt");
 	exit(1);
 }
-
+*/
+/*
 static void	handle_heredoc_input(int temp_fd, const char *delimiter,
 		t_command *command, t_parenthesis_fd *parenthesis_fd)
 {
@@ -40,6 +42,34 @@ static void	handle_heredoc_input(int temp_fd, const char *delimiter,
 		buffer = readline("heredoc> ");
 		if (buffer == NULL)
 			cleanup_and_exit(temp_fd, command, parenthesis_fd);
+		if (ft_strncmp(buffer, delimiter, ft_strlen(delimiter)) == 0)
+		{
+			free(buffer);
+			break ;
+		}
+		write(temp_fd, buffer, ft_strlen(buffer));
+		write(temp_fd, "\n", 1);
+		free(buffer);
+	}
+}
+*/
+
+static void	handle_heredoc_input(int temp_fd, const char *delimiter,
+		t_command *command, t_parenthesis_fd *parenthesis_fd)
+{
+	char	*buffer;
+	(void)command;
+	(void)parenthesis_fd;
+
+	while (1)
+	{
+		buffer = readline("heredoc> ");
+		if (buffer == NULL)  // Ctrl+D pressed
+		{
+			// Send the content collected so far to the command and break
+			write(temp_fd, "\n", 1);  // Optionally send a newline before exit
+			break ;
+		}
 		if (ft_strncmp(buffer, delimiter, ft_strlen(delimiter)) == 0)
 		{
 			free(buffer);
