2ea6cbdd0960537214e148bcb5a0bc3636c0708e
Emil Ebert <emilebert@icloud.com>
2025-01-19 16:54:10 +0100

    fix exit command

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,62 +6,37 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 20:53:37 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/17 19:01:39 by eebert           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   exit.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ftapponn <ftapponn@student.42heilbronn.de  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/13 21:11:24 by ftapponn          #+#    #+#             */
-/*   Updated: 2025/01/12 19:50:54 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/19 16:52:37 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	validate_exit_arguments(char *argument, char ***cmd)
+static int	validate_exit_arguments(char** argv)
 {
 	int	i;
 
-	*cmd = ft_split(argument, ' ');
-	if (!*cmd)
-		return (pec(ERROR_SPLIT));
-	if ((*cmd)[1])
-	{
-		free_string_array(*cmd);
+	if ((argv)[2])
 		return (pec(ERROR_TOO_ARGUMENT));
-	}
 	i = -1;
-	while ((*cmd)[0][++i] != '\0')
+	while ((argv)[1][++i] != '\0')
 	{
-		if (!ft_isdigit((*cmd)[0][i]))
-		{
-			free_string_array(*cmd);
+		if (!ft_isdigit((argv)[1][i]))
 			return (pec(ERROR_DIGIT_ARGUMENT));
-		}
 	}
 	return (0);
 }
 
 int	ft_exit(t_command *command)
 {
-	char	**cmd;
-
-	if (command->argument == NULL)
+	if (command->argv[1] == NULL)
 	{
 		get_shell()->exit_status = EXIT_SUCCESS;
 		get_shell()->should_exit = true;
 		return (0);
 	}
-	if (validate_exit_arguments(command->argument, &cmd) != 0)
+	if (validate_exit_arguments(command->argv) != 0)
 		return (-1);
-	get_shell()->exit_status = ft_atol(cmd[0]) % 255;
-	free_string_array(cmd);
 	get_shell()->should_exit = true;
-	return (0);
+	return (ft_atol(command->argv[1]) % 255);
 }
