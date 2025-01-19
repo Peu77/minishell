54c612c84d153acb713c4193564eaf2741821692
Emil Ebert <emilebert@icloud.com>
2025-01-19 10:57:32 +0100

    fxied oldpwd and pwd env

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:37:15 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/18 23:27:02 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/19 10:54:14 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,24 @@
 int	ft_cd(t_command *command)
 {
 	char	cwd[PATH_MAX];
+	char* oldpwd;
 
 	getcwd(cwd, sizeof(cwd));
 	if(command->argv[2] != NULL)
 		return (pec("cd : Too much argument"));
 	if (ft_strncmp(command->argv[1], "-", 2) == 0)
 	{
-		if (chdir(get_env_value("OLDPWD")) != 0)
+		oldpwd = get_env_value("OLDPWD");
+		if(!oldpwd)
+			return (pec("OLDPWD not set"));
+		if (chdir(oldpwd) != 0)
 			return (pec(ERROR_PATH));
+		printf("%s\n", oldpwd);
 		return (set_env_value("PWD", get_env_value("OLDPWD")), set_env_value("OLDPWD", cwd), 0);
 	}
 	if (chdir(command->argv[1]) != 0)
 		return (pec(ERROR_PATH));
-	set_env_value("PWD", command->argv[1]);
-	return (set_env_value("OLDPWD", cwd), 0);
+	set_env_value("OLDPWD", cwd);
+	getcwd(cwd, sizeof(cwd));
+	return (set_env_value("PWD", cwd), 0);
 }
