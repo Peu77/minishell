56bbe57baa93cc4af1b3f17b89d3a3bd3f1cd9a1
Emil Ebert <emilebert@icloud.com>
2025-01-20 00:54:08 +0100

    fix exit

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 20:53:37 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/20 00:32:24 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/20 00:53:21 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static int	validate_exit_arguments(char** argv)
 	size_t	i;
 
 	i = -1;
+	if(argv[1][0] == 0)
+		return (pe(ERROR_DIGIT_ARGUMENT), 255);
 	while ((argv)[1][++i] != '\0')
 	{
 		if (!ft_isdigit((argv)[1][i]))
@@ -33,6 +35,7 @@ int	ft_exit(t_command *command)
 {
 	int validate_exit_status;
 
+	get_shell()->should_exit = true;
 	if (command->argv[1] == NULL)
 	{
 		get_shell()->exit_status = EXIT_SUCCESS;
@@ -42,6 +45,5 @@ int	ft_exit(t_command *command)
 	validate_exit_status = validate_exit_arguments(command->argv);
 		if(validate_exit_status != 0)
 			return (validate_exit_status);
-	get_shell()->should_exit = true;
 	return (ft_atoi(command->argv[1]) % 256);
 }
