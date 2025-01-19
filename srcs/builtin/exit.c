c137bd78e48ed7e71ed37268f7bb09a2a0c7ecda
Emil Ebert <emilebert@icloud.com>
2025-01-20 00:33:12 +0100

    fix exit fail code

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 20:53:37 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/20 00:27:06 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/20 00:32:24 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,34 @@
 
 static int	validate_exit_arguments(char** argv)
 {
-	int	i;
+	size_t	i;
 
-	if ((argv)[2])
-		return (pec(ERROR_TOO_ARGUMENT));
 	i = -1;
 	while ((argv)[1][++i] != '\0')
 	{
 		if (!ft_isdigit((argv)[1][i]))
-			return (pec(ERROR_DIGIT_ARGUMENT));
+			return (pe(ERROR_DIGIT_ARGUMENT), 255);
 	}
+
+	if ((argv)[2])
+		return (pec(ERROR_TOO_ARGUMENT));
+
 	return (0);
 }
 
 int	ft_exit(t_command *command)
 {
+	int validate_exit_status;
+
 	if (command->argv[1] == NULL)
 	{
 		get_shell()->exit_status = EXIT_SUCCESS;
 		get_shell()->should_exit = true;
 		return (0);
 	}
-	if (validate_exit_arguments(command->argv) != 0)
-		return (1);
+	validate_exit_status = validate_exit_arguments(command->argv);
+		if(validate_exit_status != 0)
+			return (validate_exit_status);
 	get_shell()->should_exit = true;
 	return (ft_atoi(command->argv[1]) % 256);
 }
