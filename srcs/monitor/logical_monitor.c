0b65226a82a491c89c17cf2902539a7ddc93bf4e
Emil Ebert <emilebert@icloud.com>
2025-01-20 16:20:08 +0100

    implement semicolon

--- srcs/monitor/logical_monitor.c
+++ srcs/monitor/logical_monitor.c
@@ -1,6 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
+/*   logical_monitor.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 20:35:30 by eebert            #+#    #+#             */
+/*   Updated: 2025/01/20 16:14:27 by eebert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
 /*   and_or_monitor.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ftapponn <ftapponn@student.42heilbronn.de  +#+  +:+       +#+        */
@@ -32,6 +44,18 @@ int	or_monitor(t_ast_node *node, t_command *command)
 	if (node->left)
 		left_result = tree_monitor(node->left, command);
 	if (left_result && node->right)
+		return (tree_monitor(node->right, command));
+	return (left_result);
+}
+
+int	semicolon_monitor(t_ast_node *node, t_command *command)
+{
+	int	left_result;
+
+	left_result = 0;
+	if (node->left)
+		left_result = tree_monitor(node->left, command);
+	if (node->right)
 		return (tree_monitor(node->right, command));
 	return (left_result);
 }
