0f59b11f2856a0861e34980121527961c0133759
flotapponnier <florent.tapponnier@gmail.com>
2025-01-20 22:29:45 +0100

    pipe fd

--- srcs/monitor/pipe_monitor.c
+++ srcs/monitor/pipe_monitor.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 18:23:59 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/20 21:29:01 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/20 22:29:02 by ftapponn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,17 @@
 
 void	pipe_left_process(t_ast_node *node, t_pipe_data *pipe_data)
 {
-	close(pipe_data->pipe_fds[0]);
+	gc_close_fd(pipe_data->pipe_fds[0]);
 	dup2(pipe_data->pipe_fds[1], STDOUT_FILENO);
-	close(pipe_data->pipe_fds[1]);
+	gc_close_fd(pipe_data->pipe_fds[1]);
 	destroy_minishell(tree_monitor(node->left, NULL));
 }
 
 void	pipe_right_process(t_ast_node *node, t_pipe_data *pipe_data)
 {
-	close(pipe_data->pipe_fds[1]);
+	gc_close_fd(pipe_data->pipe_fds[1]);
 	dup2(pipe_data->pipe_fds[0], STDIN_FILENO);
-	close(pipe_data->pipe_fds[0]);
+	gc_close_fd(pipe_data->pipe_fds[0]);
 	pipe_data->right_result = tree_monitor(node->right, NULL);
 	destroy_minishell(pipe_data->right_result);
 }
@@ -47,8 +47,8 @@ void	pipe_right_process(t_ast_node *node, t_pipe_data *pipe_data)
 int	pipe_fork_error(t_pipe_data *pipe_data)
 {
 	kill(pipe_data->left_pid, SIGKILL);
-	close(pipe_data->pipe_fds[0]);
-	close(pipe_data->pipe_fds[1]);
+	gc_close_fd(pipe_data->pipe_fds[0]);
+	gc_close_fd(pipe_data->pipe_fds[1]);
 	return (pec("Fork for right process failed"));
 }
 
@@ -111,8 +111,8 @@ int	pipe_monitor(t_ast_node *node)
 		return (pipe_fork_error(&pipe_data));
 	if (pipe_data.right_pid == 0)
 		pipe_right_process(node, &pipe_data);
-	close(pipe_data.pipe_fds[0]);
-	close(pipe_data.pipe_fds[1]);
+	gc_close_fd(pipe_data.pipe_fds[0]);
+	gc_close_fd(pipe_data.pipe_fds[1]);
 	waitpid(pipe_data.left_pid, NULL, 0);
 	waitpid(pipe_data.right_pid, &pipe_data.right_status, 0);
 	main_signals();
