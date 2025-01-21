d7ba3b3e69a735631ce298d5b99875a03c4764c5
Florent Tapponnier <ftapponn@3-I-5.42heilbronn.de>
2025-01-21 16:33:54 +0100

    clean monitor

--- srcs/monitor/pipe_monitor.c
+++ srcs/monitor/pipe_monitor.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 18:23:59 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/21 13:35:06 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/21 16:32:33 by ftapponn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,42 +52,14 @@ int	pipe_fork_error(t_pipe_data *pipe_data)
 	return (pec("Fork for right process failed"));
 }
 
-/*
-void	print_node_left(t_ast_node *node) {
-	if (node) {
-		// First, check and print information about the left node
-		if (node->left) {
-			printf("Left node - value: %s, type: %d\n", node->left->value,
-				node->left->type);
-
-			// Now, check and print the redirects for the left node
-			if (node->left->redirects) {
-				t_list *current_redirect = node->left->redirects;
-
-				// Iterate over the list of redirects
-				while (current_redirect) {
-					// Cast the content of the current list node to t_redirect
-					t_redirect *redirect = (t_redirect *)current_redirect->content;
-
-					// Print the details of the current redirect
-					printf("Redirect (left node) - fd_left: %d, fd_right: %d,
-						file: %s, type: %d\n",
-							redirect->fd_left, redirect->fd_right,
-								redirect->file, redirect->type);
-
-					// Move to the next redirect in the list
-					current_redirect = current_redirect->next;
-				}
-			} else {
-				printf("No redirects in the left node.\n");
-			}
-
-		} else {
-			printf("No left node.\n");
-		}
-	}
+static void	close_pipe_and_wait(t_pipe_data *pipe_data)
+{
+	gc_close_fd(pipe_data->pipe_fds[0]);
+	gc_close_fd(pipe_data->pipe_fds[1]);
+	waitpid(pipe_data->left_pid, NULL, 0);
+	waitpid(pipe_data->right_pid, &pipe_data->right_status, 0);
+	main_signals();
 }
-*/
 
 int	pipe_monitor(t_ast_node *node)
 {
@@ -104,21 +76,14 @@ int	pipe_monitor(t_ast_node *node)
 	pipe_data.left_pid = fork();
 	if (pipe_data.left_pid == -1)
 		return (pec("Fork for left process failed"));
-	// print_node_left(node);
 	if (pipe_data.left_pid == 0)
-	{
 		pipe_left_process(node, &pipe_data);
-	}
 	pipe_data.right_pid = fork();
 	if (pipe_data.right_pid == -1)
 		return (pipe_fork_error(&pipe_data));
 	if (pipe_data.right_pid == 0)
 		pipe_right_process(node, &pipe_data);
-	gc_close_fd(pipe_data.pipe_fds[0]);
-	gc_close_fd(pipe_data.pipe_fds[1]);
-	waitpid(pipe_data.left_pid, NULL, 0);
-	waitpid(pipe_data.right_pid, &pipe_data.right_status, 0);
-	main_signals();
+	close_pipe_and_wait(&pipe_data);
 	if (WIFEXITED(pipe_data.right_status))
 		pipe_data.right_result = WEXITSTATUS(pipe_data.right_status);
 	get_shell()->exit_status = pipe_data.right_result;
