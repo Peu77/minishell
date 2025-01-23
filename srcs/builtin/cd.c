11755830ac11ebe09af374a22d7b24d041d3a013
Emil Ebert <emilebert@icloud.com>
2025-01-23 10:38:20 +0100

    fix cd

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,36 +6,46 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 17:37:15 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/21 13:28:15 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/23 10:36:52 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include <limits.h>
 
-int	ft_cd(t_command *command)
+static int	change_directory(char *path)
 {
 	char	cwd[PATH_MAX];
-	char	*oldpwd;
 
 	getcwd(cwd, sizeof(cwd));
-	if (command->argv[2] != NULL)
+	if (chdir(path) != 0)
+		return (pec(ERROR_PATH));
+	set_env_value("OLDPWD", cwd);
+	getcwd(cwd, sizeof(cwd));
+	set_env_value("PWD", cwd);
+	return (0);
+}
+
+int	ft_cd(t_command *command)
+{
+	char	*oldpwd;
+
+	if (command->argv[1] != NULL && command->argv[2] != NULL)
 		return (pec("cd : Too much argument"));
+	if (!command->argv[1])
+	{
+		if (!get_env_value("HOME") || !*get_env_value("HOME"))
+			return (pec("HOME not set"));
+		return (change_directory(get_env_value("HOME")));
+	}
 	if (ft_strncmp(command->argv[1], "-", 2) == 0
 		|| ft_strncmp(command->argv[1], "--", 3) == 0)
 	{
 		oldpwd = get_env_value("OLDPWD");
 		if (!oldpwd || !*oldpwd)
 			return (pec("OLDPWD not set"));
-		if (chdir(oldpwd) != 0)
-			return (pec(ERROR_PATH));
 		printf("%s\n", oldpwd);
-		return (set_env_value("PWD", get_env_value("OLDPWD")),
-			set_env_value("OLDPWD", cwd), 0);
+		return (change_directory(oldpwd));
 	}
-	if (chdir(command->argv[1]) != 0)
-		return (pec(ERROR_PATH));
-	set_env_value("OLDPWD", cwd);
-	getcwd(cwd, sizeof(cwd));
-	return (set_env_value("PWD", cwd), 0);
+	return (change_directory(command->argv[1]));
 }
