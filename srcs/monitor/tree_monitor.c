30b3df51c327520af5870173ef575bd6ee6c5dcb
Emil Ebert <emilebert@icloud.com>
2025-01-24 16:20:02 +0100

    heredoc traverse

--- srcs/monitor/tree_monitor.c
+++ srcs/monitor/tree_monitor.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 09:25:23 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/21 13:34:39 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/24 15:50:08 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,33 @@ int	tree_monitor(t_ast_node *node, t_command *command)
 	if (node->type == AST_COMMAND)
 		return (command_monitor(node, command));
 	return (0);
+}
+
+bool traverse_heredocs(t_ast_node *node) {
+	t_list * redirects;
+	t_list* tmp;
+	int exit_code;
+
+	redirects = NULL;
+	if (!node)
+		return (true);
+	if (node->type != AST_COMMAND) {
+		if (!traverse_heredocs(node->left))
+			return (false);
+		if (!traverse_heredocs(node->right))
+			return (false);
+		return true;
+	}
+
+	gc_free_ptr(filter_and_get_redirects(node->value, &redirects, &exit_code));
+	tmp = redirects;
+	while (tmp) {
+		if (((t_redirect*) tmp->content)->type == TOKEN_REDIRECT_INPUT_APPEND) {
+			 redirection_heredoc(((t_redirect*) tmp->content)->file, node);
+		}
+		tmp = tmp->next;
+	}
+
+	gc_list_clear(&redirects, free_redirect);
+	return true;
 }
