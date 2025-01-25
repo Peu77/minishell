b05617edf100607817d8d6408125b485d7a94617
Emil Ebert <emilebert@icloud.com>
2025-01-25 18:01:00 +0100

    refactor expansion code to ast_utils

--- srcs/parser/ast_utils.c
+++ srcs/parser/ast_utils.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 13:15:33 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/25 16:32:23 by eebert           ###   ########.fr       */
+/*   Updated: 2025/01/25 17:36:52 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,36 @@ t_ast_node	*create_ast_node(t_ast_type type, char *value, t_list *redirects)
 	node->left = NULL;
 	node->right = NULL;
 	return (node);
+}
+
+bool	parse_redirects_from_node(t_ast_node *node, int *redirect_exit_code)
+{
+	char	*tmp;
+
+	tmp = filter_and_get_redirects(node->value, &node->redirects,
+			redirect_exit_code);
+	gc_free_ptr(node->value);
+	node->value = tmp;
+	if (*redirect_exit_code != 0)
+	{
+		get_shell()->exit_status = *redirect_exit_code;
+		if (*redirect_exit_code == 258)
+			pe("parse error, each redirect should have an filename");
+		return (1);
+	}
+	return (0);
+}
+
+void	expand_ast_node(t_ast_node *node)
+{
+	char	*tmp;
+
+	if(node->value == NULL)
+		return ;
+	tmp = expand_string(node->value);
+	gc_free_ptr(node->value);
+	node->value = tmp;
+	tmp = expand_wildcards(node->value);
+	gc_free_ptr(node->value);
+	node->value = tmp;
 }
