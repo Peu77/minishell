99c5f311158b2e809dddadf78bec888cb1117841
Emil Ebert <emilebert@icloud.com>
2025-01-25 18:01:06 +0100

    refactor expansion code to ast_utils

--- srcs/monitor/command_monitor.c
+++ srcs/monitor/command_monitor.c
@@ -6,42 +6,12 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:30:09 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/24 16:56:21 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/25 17:37:47 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include "parse.h"
-
-static void	process_expansions(t_ast_node *node)
-{
-	char	*tmp;
-
-	tmp = expand_string(node->value);
-	gc_free_ptr(node->value);
-	node->value = tmp;
-	tmp = expand_wildcards(node->value);
-	gc_free_ptr(node->value);
-	node->value = tmp;
-}
-
-static int	handle_redirects(t_ast_node *node, int *redirect_exit_code)
-{
-	char	*tmp;
-
-	tmp = filter_and_get_redirects(node->value, &node->redirects,
-			redirect_exit_code);
-	gc_free_ptr(node->value);
-	node->value = tmp;
-	if (*redirect_exit_code != 0)
-	{
-		get_shell()->exit_status = *redirect_exit_code;
-		if (*redirect_exit_code == 258)
-			pe("parse error, each redirect should have an filename");
-		return (1);
-	}
-	return (0);
-}
 
 static char	**process_arguments(t_ast_node *node)
 {
@@ -76,8 +46,8 @@ int	command_monitor(t_ast_node *node, t_command *command)
 	int		redirect_exit_code;
 
 	redirect_exit_code = 0;
-	process_expansions(node);
-	if (handle_redirects(node, &redirect_exit_code))
+	expand_ast_node(node);
+	if (parse_redirects_from_node(node, &redirect_exit_code))
 		return (redirect_exit_code);
 	argv = process_arguments(node);
 	return (execute_transformed_command(node, command, argv));
