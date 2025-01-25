254500ff87a1033e3200b0a11d53709adaf7c80d
Emil Ebert <emilebert@icloud.com>
2025-01-25 18:01:20 +0100

    update gc_files so it can't create memory leaks

--- srcs/gcollector/gc_files.c
+++ srcs/gcollector/gc_files.c
@@ -6,7 +6,7 @@
 /*   By: eebert <eebert@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:25:26 by eebert            #+#    #+#             */
-/*   Updated: 2025/01/25 14:17:40 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/01/25 17:52:16 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ void	gc_close_fds(void)
 	{
 		next = current->next;
 		close(*((int *)current->content));
-		free(current->content);
-		free(current);
+		gc_free_ptr(current->content);
+		gc_free_ptr(current);
 		current = next;
 	}
 }
@@ -43,13 +43,9 @@ int	gc_add_fd(int fd)
 
 	if (fd < 0)
 		return (-1);
-	fd_ptr = malloc(sizeof(int));
-	if (!fd_ptr)
-		destroy_minishell(EXIT_FAILURE);
+	fd_ptr = gc_malloc(sizeof(int));
 	*fd_ptr = fd;
-	new_node = ft_lstnew(fd_ptr);
-	if (!new_node)
-		destroy_minishell(EXIT_FAILURE);
+	new_node = gc_add(ft_lstnew(fd_ptr));
 	ft_lstadd_back(get_gc_head_files(), new_node);
 	return (fd);
 }
@@ -72,8 +68,8 @@ void	gc_close_fd(int fd)
 			else
 				*get_gc_head_files() = current->next;
 			close(fd);
-			free(current->content);
-			free(current);
+			gc_free_ptr(current->content);
+			gc_free_ptr(current);
 			return ;
 		}
 		previous = current;
